a6a38890a059db6d01a977b7afff517c82725012
Connor <cnnrznn@udel.edu>
2016-01-10 21:51:55 -0500

    Progress, progress

--- echoserver.py
+++ echoserver.py
@@ -7,8 +7,13 @@ import signal
 # intercept signal
 def sigint_handler(signal, frame):
     print "Cleaning up sockets..."
-    # TODO close sockets in other lists
-    msock.close()
+    # close all open sockets
+    for sock in readable:
+        sock.close()
+    for sock in writable:
+        sock.close()
+    for sock in errable:
+        sock.close()
     print "Done!"
     exit(0)
 signal.signal(signal.SIGINT, sigint_handler)
@@ -18,17 +23,31 @@ msock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 msock.bind(("", 1024))
 print "Successfully binded to:", msock.getsockname()
 
+# start listening
+msock.listen(5)
+print "Listening on:", msock.getsockname()
+
+# open socket lists
 readable = [msock]
 writable = []
 errable = []
 
 while 1:
-    select.select(
-        readable,
-        writable,
-        errable,
-        60)
+    tmpr, tmpw, tmpe = select.select(
+                        readable,
+                        writable,
+                        errable,
+                        60)
 
-    # TODO handle socket read either
+    # handle socket read either
     #   1. accept() new connection
     #   2. read data from socket
+
+    if msock in tmpr:
+        newconn, addr = msock.accept()
+        readable.append(newconn)
+        tmpr.remove(msock)
+
+    # read from every connection and echo
+    for sock in tmpr:
+        sock.send(sock.recv(1024))
